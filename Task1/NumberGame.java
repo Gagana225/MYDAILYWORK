eecdd18c1175e5aa17df4da86b7dbb561007de1f
Gagana Shetty <gaganashetty56@gmail.com>
2026-02-06 15:30:39 +0530

    Add Number Guessing Game code

--- Task1/NumberGame.java
+++ Task1/NumberGame.java
@@ -1,1 +1,39 @@
 
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        int number = random.nextInt(100) + 1;
+        int guess = 0;
+        int attempts = 0;
+        int maxAttempts = 5;
+
+        System.out.println("Welcome to Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 100");
+        System.out.println("You have " + maxAttempts + " attempts.");
+
+        while (guess != number && attempts < maxAttempts) {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess > number) {
+                System.out.println("Too high!");
+            } else if (guess < number) {
+                System.out.println("Too low!");
+            } else {
+                System.out.println("Correct! You guessed in " + attempts + " attempts.");
+            }
+        }
+
+        if (guess != number) {
+            System.out.println("Out of attempts! The number was: " + number);
+        }
+
+        sc.close();
+    }
+}
